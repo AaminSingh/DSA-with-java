05f1d1df22d1020209b1133ab8e7d96c229500e9
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-08-03 21:21:09 +0530

    adding the file

---
+++ Newclass.java
@@ -0,0 +1,76 @@
+import java.util.*;
+/*public class Calculator{
+
+public static void main(String[] args){    
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+
+int button = sc.nextInt();
+
+switch(button){
+    case 1:System.out.println(a+b);
+    break;
+    case 2:System.out.println(a-b);
+    break;
+    case 3:System.out.println(a*b);
+    break;
+    case 4:System.out.println(a%b);
+    break;
+    case 5:System.out.println(a/b);
+    break;
+    default:System.out.println("Invalid calculation");
+}
+}}*/
+
+ public class Newclass{
+     public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+
+//        int n= sc.nextInt();
+
+
+    //     for(int i=1;i<=n;i++){
+    //       System.out.println("Apna clg");
+    //     }
+
+
+
+    //    for (int i=1;i<=n;i++){
+    //    if(i%2 ==0 ){
+    //    System.out.println(i);
+    //    }}
+
+        
+
+    
+       
+        //int choice;
+    //    do{
+    //        System.out.println("Enter 1 or 0");
+    //        choice = sc.nextInt();
+
+    //        if(choice == 1){
+    //            System.out.println("Enter the marks");
+    //            int marks = sc.nextInt();
+
+    //            if(marks>=90){
+    //                System.out.println("This is Good");
+    //            }
+    //            else if(marks >60 && marks <=89){
+    //                System.out.println("This is also Good");
+    //            }
+    //            else if(marks >0 && marks <59){
+    //                System.out.println("This is Good as well");
+    //            }
+    //        }
+    //    }while(choice != 0);
+    //    sc.close();
+
+// 
+     }
+
+ }
+
+
+     
