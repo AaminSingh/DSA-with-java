2e2c663b721da337c39097234f78914d6d583e57
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-08-07 17:42:08 +0530

    adding the subarrays problem solution code

--- arrays/subarrays.java
+++ arrays/subarrays.java
@@ -2,6 +2,7 @@ package arrays;
 
 public class subarrays {
     public static void Subarrays(int nums[]){
+        int totalsubarrays = 0;
         for(int i=0;i<nums.length;i++){
             int start = i;
             for(int j=i;j<nums.length-1;j++){
@@ -9,10 +10,12 @@ public class subarrays {
                 for(int k=start;k<=end;k++){        //only to print
                     System.out.print(nums[k]+" ");
                 }
+                totalsubarrays++;
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("total subarrays"+ totalsubarrays);
     }
     public static void main(String args[]){
         int nums[] = {2,4,6,8,10};
