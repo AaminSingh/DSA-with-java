f40812d1cc41435b1ceecae95666b6f74d9e21b4
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-08-17 21:49:53 +0530

    solution for kadanealgo problem

---
+++ arrays/kadanesalgorithm.java
@@ -0,0 +1,23 @@
+package arrays;
+import java.util.*;
+
+
+public class kadanesalgorithm {
+    public static void Kadanealgo(int nums[]){
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+        for(int i=0;i<nums.length;i++){
+            cs = cs + nums[i];
+
+            if(cs < 0){
+                cs = 0;
+            }
+            ms = Math.max(cs,ms);
+        }
+        System.out.println("Our maximum subarray is: " + ms);
+    }
+    public static void main(String args[]){
+        int nums[] = {-2,-3,4,-1,-2,1,5,-3};
+         Kadanealgo(nums);
+    }
+}
