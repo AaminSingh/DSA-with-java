c0a7f7a6655ddb1bdcd2dc994a9a2f1d9ef7d07d
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2026-08-30 12:06:27 +0530

    solution for rotate array

---
+++ strivers_array/rotatearrayusingHash.java
@@ -0,0 +1,30 @@
+package strivers_array;
+
+public class rotatearrayusingHash {
+      private static  void reverse(int[] nums, int left, int right){
+           while(left<right){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+           }
+      }
+
+       public  static void rotateArrayusingHash(int nums[],int k){
+         int n = nums.length;
+         k = k % n;
+         reverse(nums, 0, n-1);
+         reverse(nums, 0, k-1);
+         reverse(nums,k,n-1);
+       }
+        public static void main(String args[]){
+        int nums[] = {1,2,4,5,7,8};
+        int k =2;
+        rotateArrayusingHash(nums, k);
+        for(int num: nums){
+            System.out.print(num+" ");
+        }
+        
+    }
+}
